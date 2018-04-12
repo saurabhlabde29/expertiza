41dc9b2ce8f696d3f627dd74bc15e75fe872ea71
Saurabh Labde <saurabhlabde@Saurabhs-MacBook-Pro.local>
2018-04-12 18:56:23 -0400

    Added a migration fot the duties table

---
+++ db/migrate/20180412223832_create_duties_table.rb
@@ -0,0 +1,10 @@
+class CreateDutiesTable < ActiveRecord::Migration
+  def change
+    create_table :duties do |t|
+    	t.string :name
+    	t.integer :instructor_id
+    	t.integer :course_id
+    	t.integer :user_id
+    end
+  end
+end
